a40596fb483eb1f3cff20aed827a06265ee022ac
Aldi Dharmawan <aldidharmawan700@gmail.com>
2023-08-30 15:44:32 +0700

    Update Navbar & Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,37 +2,31 @@
 
 @section('Navbar')
     <main>
-      <div class="container">
+      {{-- <div class="container">
         <div class="d-flex justify-content-center">
         <div class="card me-5" style="width: 20em;">
-            {{-- <img src="..." class="card-img-top" alt="..."> --}}
             <div class="card-body text-center border border-5 border-dark rounded">
               <i class="fas fa-solid fa-box fa-lg mb-3"></i>
               <h1 class="card-title">45</h1>
               <a href=""><h5 class="card-text text-black">Data Barang</h5></a>
-              {{-- <a href="#" class="btn btn-success">Data Barang</a> --}}
             </div>
           </div>
 
           <div class="card me-5" style="width: 20em;">
-            {{-- <img src="..." class="card-img-top" alt="..."> --}}
             <div class="card-body text-center border border-5 border-dark rounded">
               <i class="fas fa-solid fa-building fa-lg mb-3"></i>             
               <h1 class="card-title">45</h1>
               <a href=""><h5 class="card-text text-black">Perusahaan</h5></a>
-              {{-- <a href="#" class="btn btn-success">Data Barang</a> --}}
             </div>
           </div>
 
           <div class="card me-5" style="width: 20em;">
-            {{-- <img src="..." class="card-img-top" alt="..."> --}}
             <div class="card-body text-center border border-5 border-dark rounded">
               <i class="fas fa-solid fa-user fa-lg mb-3"></i>              
               <h1 class="card-title">45</h1>
               <a href=""><h5 class="card-text text-black">Pengguna</h5></a>
-              {{-- <a href="#" class="btn btn-success">Data Barang</a> --}}
             </div>
-          </div>
+          </div> --}}
 
           
 
@@ -54,7 +48,47 @@
             </div>
           </div> --}}
 
+
+          <div class="container">
+            <h2 class="text-capitalize text-center mb-5">Overview Dashboard</h2>
+          <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col text-center">
+              <div class="card h-100 shadow-sm">
+                <div class="card-body">
+                  <div class="d-flex justify-content-center">
+                  <i class="fas fa-solid fa-building fa-lg me-2"></i>             
+                  <h5 class="card-title text-success">Perusahaan</h5></i> 
+                </div> 
+                  <p class="card-text text-success fw-bold">1234123</p>
+                </div>
+            </div>
+            </div>
+            <div class="col text-center mx-auto">
+              <div class="card shadow-sm">
+                <div class="card-body">
+                  <div class="d-flex justify-content-center">
+                  <i class="fas fa-solid fa-building fa-lg me-2"></i>             
+                  <h5 class="card-title text-success">Pengguna</h5></i> 
+                </div> 
+                  <p class="card-text text-success fw-bold">1234123</p>
+                </div>
+            </div>
+            </div>
+           
+            <div class="col">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+              </div>
+            </div>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
     </main>
 @endsection
