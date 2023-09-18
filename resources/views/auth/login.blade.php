88c39e1ba3c026ce41f13c601318cf2c4889b656
Aldi Dharmawan <aldidharmawan700@gmail.com>
2023-09-18 16:02:13 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,8 +35,73 @@
         
     </head>
 
-    <body class="authentication-bg">
-        {{-- <div class="account-pages my-5">
+    <body class="authentication-bg" style="
+    background-repeat: no-repeat;
+    height: 100vh;
+  background-size: cover;
+  background-image: linear-gradient(to top, rgba(0, 0, 0, .6), rgba(0, 0, 0, .3)), url('/template/login.jpeg');">
+        <div class="account-pages my-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="card" style="">
+                            <div class="card-body">
+                                <div class="mx-auto">
+                                    <a href="index.html">
+                                        <img class="rounded mx-auto d-block" src="template/logo.jpg" alt="Murgana" height="50" />
+                                    </a>
+                                    <h6 class="h5 mb-0 mt-3">Selamat Datang!</h6>
+                                        <p class="text-muted mt-1 mb-4">
+                                            Silahkan login terlebih dahulu untuk melanjutkan
+                                        </p>
+                                        <form action="#" class="authentication-form">
+                                            <div class="mb-3">
+                                                <label class="form-label">Email Address</label>
+                                                <div class="input-group">
+                                                    <span class="input-group-text">
+                                                        <i class="icon-dual" data-feather="mail"></i>
+                                                    </span>
+                                                    <input type="email" class="form-control" id="email" placeholder="hello@coderthemes.com">
+                                                </div>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label class="form-label">Password</label>
+                                                <div class="input-group">
+                                                    <span class="input-group-text">
+                                                        <i class="icon-dual" data-feather="lock"></i>
+                                                    </span>
+                                                    <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                                                </div>
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
+                                                    <label class="form-check-label" for="checkbox-signin">Remember me</label>
+                                                </div>
+                                            </div>
+
+                                            <div class="mb-4 text-center d-grid">
+                                                <button class="btn btn-primary" type="submit">Log In</button>
+                                            </div>
+                                            <h6 class="text-danger">Jika lupa password mohon konfirmasi ke email arga kencana@murgana.co.id</h6>
+                                            <div class="mb-3 text-center d-grid">
+                                                <button class="btn btn-success" type="submit">Konfirmasi</button>
+                                            </div>
+
+                                        </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                            
+
+    {{-- <body class="authentication-bg">
+        <div class="account-pages my-5">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-xl-10">
@@ -93,7 +158,7 @@
                                         <div class="auth-page-sidebar">
                                             <div class="auth-user-testimonial">
                                                 <img class="my-5" src="template/logo.jpg" alt="Logo Murgana">
-                                                <p class="fs-20 fw-bold text-white">Jika lupa password mohon konfirmasi ke email arga kencana@murgana.co.id</p>
+                                                <p class="fs-16 fw-bold text-white">Jika lupa password mohon konfirmasi ke email arga kencana@murgana.co.id</p>
                                                 <button class="btn border-white fw-bold text-white w-100" type="submit">Konfirmasi</button>
                                             </div>
                                         </div>
@@ -116,68 +181,6 @@
         <!-- end page -->
 
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-10">
-                    <div class="card">
-                        <div class="card-body p-0">
-                            <div class="row g-0">
-                                <div class="col-lg-6 p-5">
-                                    <h6 class="h5 mb-0 mt-3">Selamat Datang!</h6>
-                                        <p class="text-muted mt-1 mb-4">
-                                            Silahkan login terlebih dahulu untuk melanjutkan
-                                        </p>
-
-                                        <form action="#" class="authentication-form">
-                                            <div class="mb-3">
-                                                <label class="form-label">Email Address</label>
-                                                <div class="input-group">
-                                                    <span class="input-group-text">
-                                                        <i class="icon-dual" data-feather="mail"></i>
-                                                    </span>
-                                                    <input type="email" class="form-control" id="email" placeholder="example@gmail.com">
-                                                </div>
-                                            </div>
-
-                                            <div class="mb-3">
-                                                <label class="form-label">Password</label>
-                                                <div class="input-group">
-                                                    <span class="input-group-text">
-                                                        <i class="icon-dual" data-feather="lock"></i>
-                                                    </span>
-                                                    <input type="password" class="form-control" id="password" placeholder="Enter your password">
-                                                </div>
-                                            </div>
-
-                                            <div class="mb-3">
-                                                <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
-                                                    <label class="form-check-label" for="checkbox-signin">Remember me</label>
-                                                </div>
-                                            </div>
-
-                                            <div class="mb-4 text-center d-grid">
-                                                <button class="btn btn-primary" type="submit">Log In</button>
-                                            </div>
-                                        </form>
-                                </div>
-                                <div class="auth-pages col-lg-6 p-5">
-                                    <div class="auth-page-sidebar">
-                                        <div class="auth-user-testimonial">
-                                            <img class="" src="template/logo.jpg" alt="Logo Murgana">
-                                            <p class="fs-20 fw-bold text-white">Jika lupa password mohon konfirmasi ke email arga kencana@murgana.co.id</p>
-                                            <button class="btn border-white fw-bold text-white w-100" type="submit">Konfirmasi</button>
-                                        </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-              
 
         <!-- Vendor js -->
         <script src="assets/js/vendor.min.js"></script>
