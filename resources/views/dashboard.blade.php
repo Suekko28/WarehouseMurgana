215ccef72ac54d65b71dd8417f919b81008361e5
Aldi Dharmawan <aldidharmawan700@gmail.com>
2023-10-30 13:33:07 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -96,25 +96,26 @@
     <div class="card-group">
       <div class="card  me-3 shadow border">
         <div class="card-body text-center p-5">
-            <h5 class="card-title text-success"><i class="fas fa-regular fa-building  me-2"></i>Perusahaan</h5>             
-          <h4 class="card-text text-success fw-bold mt-3">1234123</h4>
+            <h5 class="card-title text-success"><i class="fas fa-regular fa-building me-2"></i><a href="/perusahaan" class="text-success">Perusahaan</h5></a>            
+            @if(auth()->user()->role==1)
+          {{-- <h4 class="card-text text-success fw-bold mt-3">1234123</h4> --}}
         </div>
       </div>
 
       <div class="card  me-3 shadow border">
         <div class="card-body text-center p-5">
-            <h5 class="card-title text-success"><i class="fas fa-solid fa-screwdriver-wrench  me-2"></i>Peralatan</h5>             
-          <h4 class="card-text text-success fw-bold mt-3">1234123</h4>
+            <h5 class="card-title text-success"><i class="fas fa-solid fa-screwdriver-wrench  me-2"></i><a href="/peralatan" class="text-success">Peralatan</h5></a>         
+          {{-- <h4 class="card-text text-success fw-bold mt-3">1234123</h4> --}}
         </div>
       </div>
 
       <div class="card  me-3 shadow border">
         <div class="card-body text-center p-5">
-            <h5 class="card-title text-success"><i class="fas fa-solid fa-user me-2"></i>Pengguna</h5>             
-          <h4 class="card-text text-success fw-bold mt-3">1234123</h4>
+            <h5 class="card-title text-success"><i class="fas fa-solid fa-user me-2"></i><a href="/pengguna" class="text-success">Pengguna</h5></a>            
+          {{-- <h4 class="card-text text-success fw-bold mt-3">1234123</h4> --}}
         </div>
       </div>
-      
+      @endif
   </div>
 
     </main>
